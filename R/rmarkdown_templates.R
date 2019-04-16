86637e493ea4e9b87c32824c609e5163559fb2e4
Adam Robinson <adamrobinson361@gmail.com>
2019-04-16 23:43:03 +0100

    Added powerpoint template

--- R/rmarkdown_templates.R
+++ R/rmarkdown_templates.R
@@ -60,3 +60,31 @@ official_statistic <- function(...) {
   rmarkdown_template("official_statistic")
 
 }
+
+#' R Markdown DfE Internal PowerPoint
+#'
+#' This function creates an R Markdown output format that generates html slide shows
+#' in the style of DfE internal presenetations. \cr
+#'
+#' To format an R Markdown document using this template add the function to the R Markdowns
+#' YAML header as follows:
+#'
+#' title: "Name" \cr
+#' output: dfeR::internal_powerpoint \cr
+#'
+#' You can also use the template in R Studio via File > New File > R Markdown > From Template ...
+#'
+#' @param ... All Parameters from rmarkdown::word_document are accepted
+#' @export
+#' @examples
+#' internal_powerpoint()
+
+internal_powerpoint <- function(){
+
+  # get the locations of resource files located within the package
+  file <- system.file("styles/internal_powerpoint.pptx", package = "dfeR")
+
+  # call the base html_document function
+  rmarkdown::powerpoint_presentation(reference_doc = file)
+
+}
